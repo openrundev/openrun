a1c155ed99b404d8e17a7d5aafe088170fdea1f5
Ajay Kidave <ajayvk@clace.io>
2025-12-01 14:01:08 -0800

    Create symlink in /usr/bin

--- deploy/setup_systemd.sh
+++ deploy/setup_systemd.sh
@@ -9,6 +9,7 @@ useradd --system --gid openrun --create-home --home-dir /var/lib/openrun \
     --shell /usr/bin/bash --comment "OpenRun" openrun
 
 su -l openrun /bin/bash -c "export OPENRUN_HOME=/var/lib/openrun; curl -sSL https://openrun.dev/install.sh | sh"
+ln -sf /var/lib/openrun/bin/openrun /usr/bin/openrun
 
 mkdir -p /etc/systemd/system
 curl -sSLo /etc/systemd/system/openrun.service https://raw.githubusercontent.com/openrundev/openrun/main/deploy/init/openrun.service
