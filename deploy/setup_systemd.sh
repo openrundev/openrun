dd3b745de1ca639678c4aa92ad277e86120f9047
Ajay Kidave <ajayvk@clace.io>
2025-12-01 14:17:41 -0800

    Add check for existing group/user

--- deploy/setup_systemd.sh
+++ deploy/setup_systemd.sh
@@ -4,9 +4,13 @@
 # Script to create the openrun user/group, install and then create the systemd service
 
 set -ex
-groupadd --system openrun
-useradd --system --gid openrun --create-home --home-dir /var/lib/openrun \
+groupadd --system -f openrun
+
+# Create user if it does not exist
+if ! id -u openrun >/dev/null 2>&1; then
+  useradd --system --gid openrun --create-home --home-dir /var/lib/openrun \
     --shell /usr/bin/bash --comment "OpenRun" openrun
+fi
 
 su -l openrun /bin/bash -c "export OPENRUN_HOME=/var/lib/openrun; curl -sSL https://openrun.dev/install.sh | sh"
 chmod +x /var/lib/openrun
